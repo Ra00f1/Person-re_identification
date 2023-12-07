25d867c585b8ce77e38f2c69ee7ec07ba397b5ae
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-12-07 21:24:59 +0300

    Test

---
+++ Pre_Processing/MARS.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import cv2
+import tenserflow as tf
+
+
+
+def Adaptive_Normalization():
+    print('Adaptive_Normalization')
+
+if __name__ == '__main__':
+    print('main')
